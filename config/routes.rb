851d9eb2bd7a71e86382eb8b994c6ce9a658981e
Moto Ikema <moto@occ.co.jp>
2017-03-09 08:56:31 +0000

    add task resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :projects
+  resources :projects do
+    resources :tasks, only: [:create, :destroy]
+  end
+
   root 'projects#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
