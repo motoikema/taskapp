bf634c7242ef53c7def4378a4da20fdba159c763
motoikema <moto@occ.co.jp>
2017-03-11 07:08:45 +0000

    fix routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,6 @@ Rails.application.routes.draw do
   end
 
   root 'projects#index'
-  root 'projects#index'
-  root 'projects#index'
-  root 'projects#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
