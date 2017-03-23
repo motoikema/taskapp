c51dba519014e7527a013daef7bef48203666091
motoikema <moto@occ.co.jp>
2017-03-23 07:34:47 +0000

    add task modules

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     resources :tasks, only: [:create, :destroy]
   end
 
+  post '/projects/:project_id/tasks/:id/toggle' => 'tasks#toggle'
+
   root 'projects#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170307070733) do
-
-  create_table "categories", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20170311062607) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
